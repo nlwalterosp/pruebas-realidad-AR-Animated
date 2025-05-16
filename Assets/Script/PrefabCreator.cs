4f0e3c1117389c00a2a28991bd819283adeaddd3
nlwalterosp <122395373+nlwalterosp@users.noreply.github.com>
2025-05-16 11:20:56 -0500

    ACTUALIZACION

---
+++ Assets/Script/PrefabCreator.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Resources;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PrefabCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject josePrefab;
+    
+    [SerializeField] private Vector3 joseonOffset;
+    
+
+    private GameObject jose;
+    
+    private ARTrackedImageManager arTrackedImageManager;
+
+    private void OnEnable()
+    {
+        arTrackedImageManager = gameObject.GetComponent<ARTrackedImageManager>();
+
+        arTrackedImageManager.trackedImagesChanged += OnImageChanged;
+    }
+
+    private void OnImageChanged(ARTrackedImagesChangedEventArgs obj)
+    {
+        foreach (ARTrackedImage image in obj.added)
+        {
+            if (image.referenceImage.name == "Target conejo prueba")
+            {
+                jose = Instantiate(josePrefab, image.transform);
+                jose.transform.position = joseonOffset;
+            }
+
+           
+        }
+
+    }
+}
---
+++ Assets/Script/josecontroller.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class josecontroller : MonoBehaviour
+{
+    private Toggle walkToggle;
+
+    private Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        walkToggle = GameObject.Find("WalkToggle").GetComponent<Toggle>();
+        
+      walkToggle.onValueChanged.AddListener(OnWalkToggleChanged);
+       
+    }
+
+    private void OnWalkToggleChanged(bool isOn)
+    {
+        animator.SetBool("Walk", isOn);
+    }
+
+   
+}
